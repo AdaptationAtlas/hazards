f61153d4ac1e514200075ae87e17db45c409ac45
jramirezv <dawnpatrolmustaine@gmail.com>
2022-12-12 22:59:31 +0000

    add mask making scripts

---
+++ R/02_preprocess_data/createPopMask.R
@@ -0,0 +1,34 @@
+#Create population mask at CHIRPS resolution, combining urban+rural
+#JRV, Dec. 2022
+
+#load packages
+library(terra)
+library(tidyverse)
+library(sf)
+
+#working directory
+wd <- "~/common_data/atlas_hazards/population_mask"
+if (!file.exists(wd)) {dir.create(wd)}
+
+#read Africa shapefile
+shp <- st_read("~/common_data/atlas_hazards/roi/africa.gpkg")
+
+#load population raster
+pop_rs <- terra::rast("~/common_data/atlas_pop/raw/cell5m_afripop2020_urbanrural_ssa_popheadcount_total.tif") %>%
+  terra::crop(., shp)
+pop_rs[pop_rs[] == 0] <- NA
+
+#resample resulting raster into CHIRPS resolution, use nn
+chirps_rs <- terra::rast("~/common_data/chirps/raw/chirps-v2.0.1995.01.01.tif") %>%
+  terra::crop(., pop_rs)
+chirps_rs[chirps_rs[]<0] <- NA
+chirps_rs[!is.na(chirps_rs[])] <- 1
+pop_rs <- terra::resample(pop_rs, chirps_rs, method="bilinear")
+
+#make zeroes as NA
+pop_rs[!is.na(pop_rs[])] <- 1
+
+#write raster
+terra::writeRaster(pop_rs, paste0(wd, "/pop_mask.tif"))
+
+
---
+++ R/02_preprocess_data/createSpamMask.R
@@ -0,0 +1,41 @@
+#Create SPAM mask at CHIRPS resolution, combining multiple crops
+#JRV, Dec. 2022
+
+#load packages
+library(terra)
+library(tidyverse)
+library(sf)
+
+#working directory
+wd <- "~/common_data/atlas_hazards/mapspam_mask"
+if (!file.exists(wd)) {dir.create(wd)}
+
+#read Africa shapefile
+shp <- st_read("~/common_data/atlas_hazards/roi/africa.gpkg")
+
+#first list all MapSPAM files labeled as "All technologies" (\\_A.tif)
+spam_dir <- "~/common_data/mapspam_2017/raw"
+spam_files <- list.files(spam_dir, pattern="\\_A.tif")
+
+#filter by _H_ (harvested area)
+spam_files <- spam_files[grep("_SSA_H_", spam_files)]
+
+#load them as raster
+spam_rs <- terra::rast(paste0(spam_dir,"/",spam_files)) %>%
+  terra::crop(., shp)
+spam_rs <- terra::app(spam_rs, fun=sum, na.rm=TRUE)
+
+#resample resulting raster into CHIRPS resolution, use nn
+chirps_rs <- terra::rast("~/common_data/chirps/raw/chirps-v2.0.1995.01.01.tif") %>%
+  terra::crop(., spam_rs)
+chirps_rs[chirps_rs[]<0] <- NA
+chirps_rs[!is.na(chirps_rs[])] <- 1
+spam_rs <- terra::resample(spam_rs, chirps_rs, method="bilinear")
+
+#make zeroes as NA
+spam_rs[!is.na(spam_rs[])] <- 1
+
+#write raster
+terra::writeRaster(spam_rs, paste0(wd, "/spam2017V2r1_allcrop_mask.tif"))
+
+
