8c1c6cf24b098e5f808219bfef5540d3a9b04b8b
haachicanoy <harold22010@gmail.com>
2022-12-12 20:23:14 -0500

    africa mask all countries

---
+++ R/01_download_data/download_chirps.R
@@ -0,0 +1,56 @@
+# Download global CHIRPS
+# By: H. Achicanoy
+# December, 2022
+
+# R options
+g <- gc(reset = T); rm(list = ls()) # Empty garbage collector
+options(warn = -1, scipen = 999)    # Remove warning alerts and scientific notation
+suppressMessages(library(pacman))
+suppressMessages(pacman::p_load(tidyverse,terra,lubridate,R.utils))
+
+# Time frame
+ini <- as.Date('1981-01-01')
+end <- as.Date('2021-12-31')
+dts <- seq(from = ini, to = end, by = 'day'); rm(ini, end)
+
+# Output directory
+Out  <- '/home/jovyan/common_data/chirps_wrld'
+dir.create(Out,F,T)
+
+# Main function
+getChirps <- function(date = dts[1]){
+  # CHIRPS base URL
+  chrps <- 'https://data.chc.ucsb.edu/products/CHIRPS-2.0/global_daily/tifs/p05'
+  # Get day and year
+  Day  <- date
+  Year <- lubridate::year(Day)
+  # Target file
+  tfile <- paste0(chrps,'/',Year,'/chirps-v2.0.',gsub('-','.',Day,fixed=T),'.tif.gz')
+  # Destination file
+  dfile <- paste0(Out,'/',basename(tfile))
+  # Raster file
+  rfile <- gsub('.gz','',dfile,fixed = T)
+  
+  if(!file.exists(rfile)){
+    
+    # Downloading
+    if(!file.exists(dfile)){
+      tryCatch(expr = {
+        utils::download.file(url = tfile, destfile = dfile)
+      },
+      error = function(e){
+        cat(paste0(basename(tfile),' failed.\n'))
+      })
+    }
+    
+    # Unzip
+    R.utils::gunzip(dfile)
+    return(cat(paste0('Image ',basename(rfile),' processed correctly!!!\n')))
+  } else {
+    return(cat(paste0('Image ',basename(rfile),' already exists!\n')))
+  }
+  
+}
+
+# Loop through the dates
+dts %>% purrr::map(.f = getChirps)
---
+++ R/02_preprocess_data/createAfricaMask.R
@@ -0,0 +1,38 @@
+## Africa mask
+## By: H. Achicanoy
+## December, 2022
+
+# R options
+g <- gc(reset = T); rm(list = ls()) # Empty garbage collector
+options(warn = -1, scipen = 999)    # Remove warning alerts and scientific notation
+suppressMessages(if(!require(pacman)){install.packages('pacman');library(pacman)} else {library(pacman)})
+suppressMessages(pacman::p_load(tidyverse,terra,sp,geodata,rnaturalearthdata,rnaturalearth))
+
+# Root directory
+root <- '/home/jovyan/common_data/atlas_hazards'
+
+## Shapefile
+# Output file
+out  <- paste0(root,'/roi/africa.gpkg')
+dir.create(dirname(out), F, T)
+if(!file.exists(out)){
+  # World shapefile
+  wrld <- rnaturalearth::ne_countries(returnclass = 'sf', scale = 50)
+  # Africa shapefile
+  afrc <- filter(wrld, region_un == 'Africa')
+  # Save shapefile in .gpkg file
+  afrc <- terra::vect(afrc)
+  terra::writeVector(afrc, out)
+}
+
+## Raster
+tif  <- paste0(root,'/roi/africa.tif')
+if(!file.exists(tif)){
+  # Africa shapefile
+  afrc <- terra::vect(out)
+  # CHIRPS template
+  ref <- terra::rast('/home/jovyan/common_data/chirps_wrld/chirps-v2.0.1981.01.02.tif')
+  ref <- ref %>% terra::crop(terra::ext(afrc))
+  ref <- terra::rasterize(afrc, ref)
+  terra::writeRaster(ref, tif)
+}
