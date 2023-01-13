900d1abb23b350d8664d09b090aad34766ce63cc
Pete Steward <p.steward@cgiar.org>
2023-01-13 11:15:27 +0300

    Trycatch added and bug fixed

--- R/chirps_sos/1.1_download_chirps.R
+++ R/chirps_sos/1.1_download_chirps.R
@@ -1,7 +1,7 @@
-SaveDir<-"/home/jovyan/common_files/chirps/raw"
+SaveDir<-"D:/GIS Resources/Climate/CHIRPS"
 
 
-DownloadCHIRPS<-function(StartYear=1980,EndYear=2021,EndDay=136,SaveDir,quiet){
+DownloadCHIRPS<-function(StartYear=1980,EndYear=2022,EndDay=365,SaveDir,quiet){
   
   URLmaster<-"https://data.chc.ucsb.edu/products/CHIRPS-2.0/africa_daily/tifs/p05/"
   
@@ -9,7 +9,6 @@ DownloadCHIRPS<-function(StartYear=1980,EndYear=2021,EndDay=136,SaveDir,quiet){
   if(!dir.exists(SaveDir)){
     dir.create(SaveDir,recursive=T)
   }
-  
   
   for(YEAR in StartYear:EndYear){ # MinYear:MaxYear (Min = 1983 Max = Present)
     
@@ -19,33 +18,46 @@ DownloadCHIRPS<-function(StartYear=1980,EndYear=2021,EndDay=136,SaveDir,quiet){
       ENDDAY<-as.numeric(format(as.Date(paste0(YEAR,"-12-31")),"%j"))
     }
     
-    for(DEKAD in 1:ENDDAY){
+    # deal with leap years
+    if(ENDDAY==365 & YEAR %in% seq(1978,2100,4)){
+      ENDDAY<-366
+    }
+    
+    for(DAY in 1:ENDDAY){
       
-      
-      if(quiet){
-        # Display progress
-        cat('\r                                                                                                                                          ')
-        cat('\r',paste0("Downloading file: ",DAY,"/",YEAR))
-        flush.console()
-      }
-      
-      
+ 
       DATE<-as.Date(paste0(YEAR,"-",DAY),format="%Y-%j")
       
       DAY<-format(DATE,"%d")
       MONTH<-format(DATE,"%m")
       
       FILE<-paste0("chirps-v2.0.",YEAR,".",MONTH,".",DAY,".tif.gz")
+      FILE2<-paste0("chirps-v2.0.",YEAR,".",MONTH,".",DAY,".tif")
       
       URL<-paste0(URLmaster,YEAR,"/",FILE)
+      URL2<-paste0(URLmaster,YEAR,"/",FILE2)
       destfile<-paste0(SaveDir,"/",FILE)
-      if(!file.exists(destfile)){
-        download.file(URL, destfile,quiet=quiet)
+      destfile2<-paste0(SaveDir,"/",FILE2)
+      
+      if(!(file.exists(destfile)|file.exists(destfile2))){
+        # Display progress
+        cat('\r                                                                                                                                          ')
+        cat('\r',paste0("Downloading file: ",FILE))
+        flush.console()
+        try(download.file(URL, destfile,quiet=quiet),silent=T)
+      }
+        
+      if(!(file.exists(destfile)|file.exists(destfile2))){
+        # Display progress
+        cat('\r                                                                                                                                          ')
+        cat('\r',paste0("Downloading file: ",FILE2))
+        flush.console()
+        try(download.file(URL2, destfile2,quiet=quiet))
       }
     }
   }
 }
 
 options(timeout = 150000)
-DownloadCHIRPS(StartYear=2019,EndYear = 2021,EndDay=365,SaveDir = SaveDir,quiet=T )
+DownloadCHIRPS(StartYear=2022,EndYear = 2022,EndDay=335,SaveDir = SaveDir,quiet=T)
 
