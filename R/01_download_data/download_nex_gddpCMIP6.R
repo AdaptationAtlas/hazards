82892f51f55ee9757e234d0d5a5933703d142994
Achicanoy Estrella <h.a.achicanoy@cgiar.org>
2024-12-17 13:59:40 -0500

    parallelizing Nex-GDDP-CMIP6 downloads

--- R/01_download_data/download_nex_gddpCMIP6.R
+++ R/01_download_data/download_nex_gddpCMIP6.R
@@ -5,7 +5,7 @@
 library(pacman)
 pacman::p_load(devtools)
 # devtools::install_github("hllauca/RClimChange")
-pacman::p_load(RClimChange)
+pacman::p_load(RClimChange,furrr,future)
 
 #define working directory
 wd <- '~/common_data/nex-gddp-cmip6'
@@ -31,20 +31,42 @@ rm(vars, gcms, ssps, prds)
 #              roi      = c(-26,58,-47,38),
 #              method   = 'curl')
 
-#download per row from setup table
-for (i in 1:nrow(stp)) {
+# #download per row from setup table
+# for (i in 1:nrow(stp)) {
+#   
+#   #initial and ending years
+#   ini <- strsplit(stp$period[i], split = '_')[[1]][1] |> as.numeric()
+#   end <- strsplit(stp$period[i], split = '_')[[1]][2] |> as.numeric()
+#   # download
+#   RClimChange::nex_download(location = wd,
+#                             model    = stp$gcm[i],
+#                             scenario = stp$ssp[i],
+#                             variable = stp$var[i],
+#                             years    = ini:end,
+#                             version  = 'v1.1',
+#                             roi      = c(-26,58,-47,38), # xmin,xmax,ymin,ymax
+#                             method   = 'curl')
+#   
+# }
+
+plan(multisession, workers = 30)
+furrr::future_map(.x = 1:nrow(stp), .f = function(.x){
   
   #initial and ending years
-  ini <- strsplit(stp$period[i], split = '_')[[1]][1] |> as.numeric()
-  end <- strsplit(stp$period[i], split = '_')[[1]][2] |> as.numeric()
+  ini <- strsplit(stp$period[.x], split = '_')[[1]][1] |> as.numeric()
+  end <- strsplit(stp$period[.x], split = '_')[[1]][2] |> as.numeric()
   # download
   RClimChange::nex_download(location = wd,
-                            model    = stp$gcm[i],
-                            scenario = stp$ssp[i],
-                            variable = stp$var[i],
+                            model    = stp$gcm[.x],
+                            scenario = stp$ssp[.x],
+                            variable = stp$var[.x],
                             years    = ini:end,
                             version  = 'v1.1',
                             roi      = c(-26,58,-47,38), # xmin,xmax,ymin,ymax
                             method   = 'curl')
+  #clean-up
+  gc(verbose = F, full = T, reset = T)
   
-}
+}, .progress = T)
+plan(sequential)
+gc(verbose = F, full = T, reset = T)
