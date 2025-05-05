f55359616d9b5b3633d603b8138be27ae31e3cac
Achicanoy Estrella <h.a.achicanoy@cgiar.org>
2025-05-05 09:59:21 -0500

    Nex-GDDP indices

--- R/04_indices/calc_NDD.R
+++ R/04_indices/calc_NDD.R
@@ -1,21 +1,22 @@
 ## Number of dry days
-## By: H. Achicanoy
-## December, 2022
+## By: H. Achicanoy, F. Castro
+## May, 2025
 
 # R options
-g <- gc(reset = T); rm(list = ls()) # Empty garbage collector
 options(warn = -1, scipen = 999)    # Remove warning alerts and scientific notation
 suppressMessages(library(pacman))
 suppressMessages(pacman::p_load(tidyverse,terra,gtools,lubridate))
 
 root <- '/home/jovyan/common_data'
 
-ref <- terra::rast(paste0(root,'/atlas_hazards/roi/africa.tif'))
+# Get CHIRPS extent
+msk <- terra::rast('/home/jovyan/common_data/chirps_wrld/chirps-v2.0.1981.01.01.tif')
+xtd <- terra::ext(msk); rm(msk)
 
-# Calculate NDD function
+# NDD function
 calc_ndd <- function(yr, mn){
   outfile <- paste0(out_dir,'/NDD-',yr,'-',mn,'.tif')
-  cat(outfile, "\n")
+  cat(outfile,'\n')
   if(!file.exists(outfile)){
     dir.create(dirname(outfile),F,T)
     # Last day of the month
@@ -23,79 +24,107 @@ calc_ndd <- function(yr, mn){
     # Sequence of dates
     dts <- seq(from = as.Date(paste0(yr,'-',mn,'-01')), to = as.Date(paste0(yr,'-',mn,'-',last_day)), by = 'day')
     # Files
-    fls <- paste0(pr_pth,'/chirps-v2.0.',gsub(pattern='-', replacement='.', x=dts, fixed=T),'.tif')
+    fls <- paste0(pr_pth, '/', 'pr_', dts, '.tif')
     fls <- fls[file.exists(fls)]
     # Read precipitation data
     prc <- terra::rast(fls)
-    prc <- prc %>% terra::crop(terra::ext(ref)) %>% terra::mask(ref)
-    prc[prc == -9999] <- NA
+    prc <- terra::crop(prc, xtd)
     # Calculate number of dry days
-    terra::app(x   = prc,
-               fun = function(x){ ndd = sum(x < 1, na.rm = T); return(ndd) },
-               filename = outfile)
+    ndd <- sum(prc < 1)
+    terra::writeRaster(x = ndd, filename = outfile)
   }
 }
 
-# # Historical setup
-# yrs <- 1995:2014
-# mns <- c(paste0('0',1:9),10:12)
-# stp <- base::expand.grid(yrs, mns) %>% base::as.data.frame(); rm(yrs,mns)
-# names(stp) <- c('yrs','mns')
-# stp <- stp %>%
-#   dplyr::arrange(yrs, mns) %>%
-#   base::as.data.frame()
-# pr_pth <- paste0(root,'/chirps_wrld') # Daily precipitation
-# out_dir <- paste0(root,'/atlas_hazards/cmip6/indices/historical/NDD')
+# # Future setup
+# for(ssp in c('ssp126', 'ssp245', 'ssp370', 'ssp585')){
+#   for(gcm in c('ACCESS-ESM1-5', 'MPI-ESM1-2-HR', 'EC-Earth3', 'INM-CM5-0', 'MRI-ESM2-0')){
+#     
+#     
+#     ## To start the process
+#     cat('----------------------', ssp, ' ', gcm, '--------------------------\n')
+#     
+#     ## Parameters
+#     cmb <- paste0(ssp, '_', gcm)
+#     yrs <- 2021:2100
+#     mnt <- c(paste0('0', 1:9), 10:12)
+#     stp <- base::expand.grid(yrs, mnt, stringsAsFactors = F) |> base::as.data.frame() |> setNames(c('yrs', 'mnt')) |> dplyr::arrange(yrs, mnt) |> base::as.data.frame(); rm(yrs, mnt)
+#     
+#     ## Setup in/out files
+#     pr_pth  <- paste0(root, '/nex-gddp-cmip6/pr/', ssp, '/', gcm)
+#     out_dir <- paste0(root, '/nex-gddp-cmip6_indices/', ssp, '_', gcm, '/NDD/') 
+#     
+#     1:nrow(stp) %>% 
+#       purrr::map(.f = function(i){calc_ndd(yr = stp$yrs[i], mn = stp$mnt[i])})
+#     
+#     ##
+#     cat('----Finish----\n')
+#     
+#   }
+# }
 # 
-# 1:nrow(stp) %>%
-#   purrr::map(.f = function(i){calc_ndd(yr = stp$yrs[i], mn = stp$mns[i])})
-
-###
-# Future setup
-gcm <- 'ACCESS-ESM1-5'
-ssp <- 'ssp585'
-prd <- '2041_2060'
-
-cmb <- paste0(ssp,'_',gcm,'_',prd)
-prd_num <- as.numeric(unlist(strsplit(x = prd, split = '_')))
-yrs <- prd_num[1]:prd_num[2]
-mns <- c(paste0('0',1:9),10:12)
-stp <- base::expand.grid(yrs, mns) %>% base::as.data.frame(); rm(yrs,mns)
-names(stp) <- c('yrs','mns')
-stp <- stp %>%
-  dplyr::arrange(yrs, mns) %>%
-  base::as.data.frame()
-pr_pth <- paste0(root,'/chirps_cmip6_africa/Prec_',gcm,'_',ssp,'_',prd) # Precipitation
-out_dir <- paste0(root,'/atlas_hazards/cmip6/indices/',cmb,'/NDD')
-
-1:nrow(stp) %>%
-  purrr::map(.f = function(i){calc_ndd(yr = stp$yrs[i], mn = stp$mns[i])})
-
-# ----------------------------------------------------------------------
-# Data fixes
-# Get reruns file. Filter by var. name Prec since NDD only uses Prec data
-source("~/Repositories/hazards/R/03_bias_correction/getReruns.R")
-other_bfiles <- c(paste0(root, "/chirps_cmip6_africa/Prec_ACCESS-ESM1-5_ssp245_2041_2060/chirps-v2.0.2043.03.18.tif"), 
-                  paste0(root, "/chirps_cmip6_africa/Prec_ACCESS-ESM1-5_ssp245_2041_2060/chirps-v2.0.2054.10.10.tif"),
-                  paste0(root, "/chirps_cmip6_africa/Prec_ACCESS-ESM1-5_ssp245_2041_2060/chirps-v2.0.2056.03.29.tif"))
-reruns_df <- getReruns(newfiles=other_bfiles) %>%
-  dplyr::filter(varname == "Prec")
-
-# Do the reruns
-for (j in 1:nrow(reruns_df)) {
-  gcm <- reruns_df$gcm[j]
-  ssp <- reruns_df$ssp[j]
-  prd <- reruns_df$prd[j]
-  cmb <- paste0(ssp,'_',gcm,'_',prd)
-  
-  #folders
-  pr_pth <- paste0(root,'/chirps_cmip6_africa/Prec_',gcm,'_',ssp,'_',prd) # Precipitation
-  out_dir <- paste0(root,'/atlas_hazards/cmip6/indices/',cmb,'/NDD')
-  
-  #remove and redo file
-  this_file <- paste0(out_dir, "/NDD-", reruns_df$yr[j], "-", reruns_df$mn[j], ".tif")
-  cat("redoing file", this_file, "\n")
-  system(paste0("rm -f ", this_file))
-  calc_ndd(yr = reruns_df$yr[j], mn = reruns_df$mn[j])
-}
-
+# 
+# 
+# 
+# # 
+# # # # Historical setup
+# # # yrs <- 1995:2014
+# # # mns <- c(paste0('0',1:9),10:12)
+# # # stp <- base::expand.grid(yrs, mns) %>% base::as.data.frame(); rm(yrs,mns)
+# # # names(stp) <- c('yrs','mns')
+# # # stp <- stp %>%
+# # #   dplyr::arrange(yrs, mns) %>%
+# # #   base::as.data.frame()
+# # # pr_pth <- paste0(root,'/chirps_wrld') # Daily precipitation
+# # # out_dir <- paste0(root,'/atlas_hazards/cmip6/indices/historical/NDD')
+# # # 
+# # # 1:nrow(stp) %>%
+# # #   purrr::map(.f = function(i){calc_ndd(yr = stp$yrs[i], mn = stp$mns[i])})
+# # 
+# # ###
+# # # Future setup
+# # gcm <- 'ACCESS-ESM1-5'
+# # ssp <- 'ssp585'
+# # prd <- '2041_2060'
+# # 
+# # cmb <- paste0(ssp,'_',gcm,'_',prd)
+# # prd_num <- as.numeric(unlist(strsplit(x = prd, split = '_')))
+# # yrs <- prd_num[1]:prd_num[2]
+# # mns <- c(paste0('0',1:9),10:12)
+# # stp <- base::expand.grid(yrs, mns) %>% base::as.data.frame(); rm(yrs,mns)
+# # names(stp) <- c('yrs','mns')
+# # stp <- stp %>%
+# #   dplyr::arrange(yrs, mns) %>%
+# #   base::as.data.frame()
+# # pr_pth <- paste0(root,'/chirps_cmip6_africa/Prec_',gcm,'_',ssp,'_',prd) # Precipitation
+# # out_dir <- paste0(root,'/atlas_hazards/cmip6/indices/',cmb,'/NDD')
+# # 
+# # 1:nrow(stp) %>%
+# #   purrr::map(.f = function(i){calc_ndd(yr = stp$yrs[i], mn = stp$mns[i])})
+# # 
+# # # ----------------------------------------------------------------------
+# # # Data fixes
+# # # Get reruns file. Filter by var. name Prec since NDD only uses Prec data
+# # source("~/Repositories/hazards/R/03_bias_correction/getReruns.R")
+# # other_bfiles <- c(paste0(root, "/chirps_cmip6_africa/Prec_ACCESS-ESM1-5_ssp245_2041_2060/chirps-v2.0.2043.03.18.tif"), 
+# #                   paste0(root, "/chirps_cmip6_africa/Prec_ACCESS-ESM1-5_ssp245_2041_2060/chirps-v2.0.2054.10.10.tif"),
+# #                   paste0(root, "/chirps_cmip6_africa/Prec_ACCESS-ESM1-5_ssp245_2041_2060/chirps-v2.0.2056.03.29.tif"))
+# # reruns_df <- getReruns(newfiles=other_bfiles) %>%
+# #   dplyr::filter(varname == "Prec")
+# # 
+# # # Do the reruns
+# # for (j in 1:nrow(reruns_df)) {
+# #   gcm <- reruns_df$gcm[j]
+# #   ssp <- reruns_df$ssp[j]
+# #   prd <- reruns_df$prd[j]
+# #   cmb <- paste0(ssp,'_',gcm,'_',prd)
+# #   
+# #   #folders
+# #   pr_pth <- paste0(root,'/chirps_cmip6_africa/Prec_',gcm,'_',ssp,'_',prd) # Precipitation
+# #   out_dir <- paste0(root,'/atlas_hazards/cmip6/indices/',cmb,'/NDD')
+# #   
+# #   #remove and redo file
+# #   this_file <- paste0(out_dir, "/NDD-", reruns_df$yr[j], "-", reruns_df$mn[j], ".tif")
+# #   cat("redoing file", this_file, "\n")
+# #   system(paste0("rm -f ", this_file))
+# #   calc_ndd(yr = reruns_df$yr[j], mn = reruns_df$mn[j])
+# # }
--- R/04_indices/calc_NTx.R
+++ R/04_indices/calc_NTx.R
@@ -1,100 +1,72 @@
-## Heat stress generic crop (NTx40)
-## By: H. Achicanoy
-## December, 2022
+## Heat stress generic crop and threshold (i.e., NTx40)
+## By: H. Achicanoy, F. Castro
+## May, 2025
 
 # R options
-g <- gc(reset = T); rm(list = ls()) # Empty garbage collector
 options(warn = -1, scipen = 999)    # Remove warning alerts and scientific notation
 suppressMessages(library(pacman))
 suppressMessages(pacman::p_load(tidyverse,terra,gtools,lubridate))
 
 root <- '/home/jovyan/common_data'
 
-ref <- terra::rast(paste0(root,'/atlas_hazards/roi/africa.tif'))
+# Get CHIRPS extent
+msk <- terra::rast('/home/jovyan/common_data/chirps_wrld/chirps-v2.0.1981.01.01.tif')
+xtd <- terra::ext(msk); rm(msk)
 
-sce_climate <- "future" #"future"
-
-# Calculate NTx function
-calc_ntx <- function(yr, mn, thr=40){
+# Calculate NDD function
+calc_ntx <- function(yr, mn, thr = 40){
   outfile <- paste0(out_dir,'/NTx',thr,'/NTx',thr,'-',yr,'-',mn,'.tif') 
   thr <- thr[!file.exists(outfile)]
   outfile <- outfile[!file.exists(outfile)]
-  if(length(outfile) > 0){
-    cat("...processing n=", length(outfile), "files for yr=", yr, "/ mn=", mn, "\n")
+  if (length(outfile) > 0) {
+    cat('...processing n=', length(outfile), 'files for yr=', yr, '/ mn=', mn, '\n')
     # Create directories
-    1:length(outfile) %>%
-      purrr::map(.f = function(j){dir.create(dirname(outfile[j]),F,T)})
+    1:length(outfile) |> purrr::map(.f = function(j){dir.create(dirname(outfile[j]),F,T)})
     # Last day of the month
     last_day <- lubridate::days_in_month(as.Date(paste0(yr,'-',mn,'-01')))
     # Sequence of dates
     dts <- seq(from = as.Date(paste0(yr,'-',mn,'-01')), to = as.Date(paste0(yr,'-',mn,'-',last_day)), by = 'day')
     # Files
-    fls <- paste0(tx_pth,'/',yr,'/Tmax.',gsub(pattern='-', replacement='.', x=dts, fixed=T),'.tif')
+    fls <- paste0(tx_pth,'/tasmax','_',dts,'.tif')
     fls <- fls[file.exists(fls)]
     # Read maximum temperature data
     tmx <- terra::rast(fls)
-    tmx <- tmx %>% terra::crop(terra::ext(ref)) %>% terra::mask(ref)
-    tmx[tmx == -9999] <- NA
+    tmx <- terra::crop(tmx, xtd)
     # Calculate heat stress generic crop
     for (j in 1:length(thr)) {
-      cat("...processing threshold thr=", thr[j], "\n")
-      terra::app(x   = tmx,
-                 fun = function(x){ ntxval = sum(x >= thr[j], na.rm = T); return(ntxval) },
-                 filename = outfile[j])
+      cat('...processing threshold thr=',thr[j],'\n')
+      ntx <- sum(tmx > thr[j])
+      terra::writeRaster(x = ntx, filename = outfile[j])
     }
-    # Clean up
-    rm(tmx)
-    gc(verbose=FALSE, full=TRUE, reset=TRUE)
+    rm(tmx); gc(F, T, T) # Clean up
   }
 }
 
-#perform calculation
-if (sce_climate == "historical") {
-  # Historical setup
-  yrs <- 1995:2014
-  mns <- c(paste0('0',1:9),10:12)
-  stp <- base::expand.grid(yrs, mns) %>% base::as.data.frame(); rm(yrs,mns)
-  names(stp) <- c('yrs','mns')
-  stp <- stp %>%
-    dplyr::arrange(yrs, mns) %>%
-    base::as.data.frame()
-  tx_pth <- paste0(root,'/chirts/Tmax') # Daily maximum temperature
-  out_dir <- paste0(root,'/atlas_hazards/cmip6/indices/historical')
-  1:nrow(stp) %>%
-    purrr::map(.f = function(i){
-        calc_ntx(yr = stp$yrs[i], mn = stp$mns[i], thr=30:50)
-        tmpfls <- list.files(tempdir(), full.names=TRUE)
-        1:length(tmpfls) %>% purrr::map(.f = function(k) {system(paste0("rm -f ", tmpfls[k]))})
-      })
-} else if (sce_climate == "future") {
-  # Future setup
-  gcm <- 'MRI-ESM2-0' #'ACCESS-ESM1-5' 'MPI-ESM1-2-HR' 'EC-Earth3' 'INM-CM5-0' 'MRI-ESM2-0'
-  for (ssp in c('ssp126', 'ssp245', 'ssp370', 'ssp585')) {
-      for (prd in c('2021_2040', '2041_2060', '2061_2080', '2081_2100')) {
-          cat("...processing gcm=", gcm, "/ ssp=", ssp, "/ period=", prd, "\n")
-          #ssp <- 'ssp245' #ssp585
-          #prd <- '2041_2060' #2021_2040
-  
-          cmb <- paste0(ssp,'_',gcm,'_',prd)
-          prd_num <- as.numeric(unlist(strsplit(x = prd, split = '_')))
-          yrs <- prd_num[1]:prd_num[2]
-          mns <- c(paste0('0',1:9),10:12)
-          stp <- base::expand.grid(yrs, mns) %>% base::as.data.frame(); rm(yrs,mns)
-          names(stp) <- c('yrs','mns')
-          stp <- stp %>%
-            dplyr::arrange(yrs, mns) %>%
-            base::as.data.frame()
-          tx_pth <- paste0(root,'/chirts_cmip6_africa/Tmax_',gcm,'_',ssp,'_',prd) # Daily maximum temperatures
-          out_dir <- paste0(root,'/atlas_hazards/cmip6/indices/',cmb)
-  
-          1:nrow(stp) %>%
-            purrr::map(.f = function(i){
-              calc_ntx(yr = stp$yrs[i], mn = stp$mns[i], thr=30:50)
-              tmpfls <- list.files(tempdir(), full.names=TRUE)
-              1:length(tmpfls) %>% purrr::map(.f = function(k) {system(paste0("rm -f ", tmpfls[k]))})
-            })
-      }
+
+# Future setup
+for(ssp in c('ssp126', 'ssp245', 'ssp370', 'ssp585')){
+  for(gcm in c('ACCESS-ESM1-5', 'MPI-ESM1-2-HR', 'EC-Earth3', 'INM-CM5-0', 'MRI-ESM2-0')){
+    
+    
+    ## To start the process
+    cat('----------------------', ssp, ' ', gcm, '--------------------------\n')
+    
+    ## Parameters
+    cmb <- paste0(ssp, '_', gcm)
+    yrs <- 2021:2100
+    mnt <- c(paste0('0', 1:9), 10:12)
+    stp <- base::expand.grid(yrs, mnt, stringsAsFactors = F) |> base::as.data.frame() |> setNames(c('yrs', 'mnt')) |> dplyr::arrange(yrs, mnt) |> base::as.data.frame(); rm(yrs, mnt)
+    
+    ## Setup in/out files
+    tx_pth  <- paste0(root, '/nex-gddp-cmip6/tasmax/', ssp, '/', gcm)
+    thr = 30:50
+    out_dir <- paste0(root, '/nex-gddp-cmip6_indices/', ssp, '_', gcm) 
+    
+    1:nrow(stp) |>
+      purrr::map(.f = function(i){calc_ntx(yr = stp$yrs[i], mn = stp$mnt[i], thr = 30)})
+    
+    ##
+    cat('----Finish----\n')
+    
   }
-} else {
-  cat("select one of historical or future for sce_climate \n")
 }
