852a2c54b3b0445d3c07773d8876862815aaa7a3
Achicanoy Estrella <h.a.achicanoy@cgiar.org>
2025-06-10 21:57:09 -0500

    new min thresh

--- R/03_bias_correction/bc_calc_anomalies_fixed.R
+++ R/03_bias_correction/bc_calc_anomalies_fixed.R
@@ -156,8 +156,8 @@ intp_anomalies <- function(his_clm, rcp_clm, anom_dir, ref, gcm_name, rcp, varna
         anom <- avg_fut - avg_his
       } else {
         # if precipitation is below or very close to zero make it 0.01
-        avg_his[avg_his[] <= 1] <- 1 # if historical has exactly zero make it non-zero, even values close to zero ### avg_his[avg_his[] == 0] <- 0.01
-        avg_fut[avg_fut[] <= 1] <- 1 # if future has exactly zero make it non-zero, even values close to zero
+        avg_his[avg_his[] <= 2] <- 2 # if historical has exactly zero make it non-zero, even values close to zero ### avg_his[avg_his[] == 0] <- 0.01
+        avg_fut[avg_fut[] <= 2] <- 2 # if future has exactly zero make it non-zero, even values close to zero
         
         #calculate anomaly in fraction
         anom <- (avg_fut - avg_his)/avg_his
